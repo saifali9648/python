58b793d88fbcb1fede9963ee3dbc4ecea48d3b71
saifali9648 <sali964876@gmail.com>
2023-12-05 11:49:04 +0530

    encapsulation

---
+++ encapsulation2.py
@@ -0,0 +1,9 @@
+class student:
+    __name="saif"
+    def __init__(self):
+        print(self.__name)
+        self.__displayinfo()
+    def __displayinfo(self):
+        print("welcome to class")
+    
+obj=student()
